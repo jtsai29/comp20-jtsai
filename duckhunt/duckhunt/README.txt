6057ba083fe5d6ff7b15381c590223048f8c4d1f
jtsai29 <jonathan.tsai@tufts.edu>
2015-10-08 11:03:52 -0400

    can display duckhunt background

---
+++ duckhunt/duckhunt/README.txt
@@ -0,0 +1,10 @@
+Jonathan Tsai
+Comp 20
+Lab 5
+README
+
+
+
+I have not collaborated or discussed this lab with anyone.
+
+I spent approximately (insert number here) hours on this lab.
--- duckhunt/duckhunt/game.js
+++ duckhunt/duckhunt/game.js
@@ -1,1 +1,13 @@
-// Replace this with your code...
+// Jonathan Tsai
+// Lab 5 Duck Hunt Code
+
+// Loads the Duck Hunt image onto page
+function init(){
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+
+	background = new Image();
+	background.src = 'duckhunt-background.gif';
+
+	ctx.drawImage(background,0,0);
+}
