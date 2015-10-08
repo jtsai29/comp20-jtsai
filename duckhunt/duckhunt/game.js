3870883ec2da2600c91d5318d76c361856a0c9ec
jtsai29 <jonathan.tsai@tufts.edu>
2015-10-08 11:19:05 -0400

    can display 1st bird

--- duckhunt/duckhunt/game.js
+++ duckhunt/duckhunt/game.js
@@ -3,11 +3,24 @@
 
 // Loads the Duck Hunt image onto page
 function init(){
+	// Sets up the canvas for display
 	canvas = document.getElementById('game_canvas');
 	ctx = canvas.getContext('2d');
 
+	// Creates and draws background
 	background = new Image();
 	background.src = 'duckhunt-background.gif';
-
 	ctx.drawImage(background,0,0);
+
+	// Draws birds into background
+	birds = new Image();
+	birds.src = 'duckhunt_various_sheet.png';
+	
+	// 1st Bird has top left corner at (40, 120)
+	// and bottom right corner at (75, 145)
+	// Width = 35, Height = 25
+	// Should be drawn into background at (85, 85)
+	ctx.drawImage(birds, 40, 120, 35, 25,
+		85, 85, 35, 25);
+
 }
