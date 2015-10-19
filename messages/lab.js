22c0ace7106e7cb0a984497ef5d5961c1e02650a
jtsai29 <jonathan.tsai@tufts.edu>
2015-10-19 08:21:11 -0400

    added msg display code and css styling

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here...
+//get JSON data
+jsondata = new XMLHttpRequest();
+jsondata.open("GET", "data.json");
+jsondata.send();
+parsed = JSON.parse(jsondata);
+
+//display JSON data in "messages" div
+msg = document.getElementById("messages");
+for (count = 0; count < parsedObjects.length; count++){
+	msg.innerHTML += "<p>" + parsedObjects[count]["content"] +
+		" " + parsedObjects[count]["username"] + "</p>";
+}
